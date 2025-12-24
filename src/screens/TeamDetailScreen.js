9cd0a46bc091cbf3a2f5e4c43274c28aa195acda
AJaswani39 <jaswanianagha8@gmail.com>
2025-12-23 19:19:48 -0700

    Adding more details

--- src/screens/TeamDetailScreen.js
+++ src/screens/TeamDetailScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
 import { TEAMS } from '../data/teams';
 import { ROSTERS } from '../data/rosters';
 import { league } from '../engine/LeagueEngine';
